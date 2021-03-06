d06f29e323007fe85d5351d295176812b0720e33
AnjaliS1 <anjalisamra1998@gmail.com>
2021-03-06 23:26:45 +0000

    created status bar

---
+++ Status_bar.py
@@ -0,0 +1,41 @@
+start = 0
+def statusbar1():
+    if diet == meat:
+        statusbar = start + 5
+    elif diet == vegetarian:
+        statusbar = + 2
+    return statusbar
+
+def statusbar2():
+    if energy == high:
+        statusbar = statusbar + 20
+    elif energy == medium:
+        statusbar = + 10
+    elif energy == low:
+        statusbar = + 5
+    return statusbar
+
+def statusbar3():
+    if commute == long:
+        statusbar = statusbar + 10
+    elif commute ==short:
+        statusbar = statusbar + 5
+    return statusbar
+
+def statusbar4():
+    if city == urban:
+        statusbar = statusbar + 10
+    elif city == rural:
+        statusbar = statusbar + 5
+    return statusbar
+
+def statusbar5():
+    if clothing purchase == high:
+        statusbar = statusbar + 20
+    elif clothing purchase == medium:
+        statusbar = statusbar + 10
+    elif clothing purchase == low:
+        statusbar = statusbar + 5
+    return statubar 
+
+# status bar edited
