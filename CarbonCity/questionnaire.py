10a18250b7090c460ddf7c9eb4b288e2ebc288ac
Chinene <73177313+Chinene-C@users.noreply.github.com>
2021-03-06 20:07:40 +0000

    reformatted

--- CarbonCity/questionnaire.py
+++ CarbonCity/questionnaire.py
@@ -1,3 +1,5 @@
-inputname = ''
-print("Hello ''! Welcome to Carbon City, where citizens strive each day to make more sustainable choices and reduce their carbon footprint")
+name = ''
+print("""Hello ''! Welcome to Carbon City, 
+where citizens strive each day to make more sustainable choices
+ and reduce their carbon footprint""")
 print("To help us calculate your carbon footprint, please answer the following questions:")
