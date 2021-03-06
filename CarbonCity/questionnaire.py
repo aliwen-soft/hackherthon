e2b7880d62c7bd7150612a4d1c8d5f3a98ceb815
Chinene <73177313+Chinene-C@users.noreply.github.com>
2021-03-06 21:30:55 +0000

    login function created

--- CarbonCity/questionnaire.py
+++ CarbonCity/questionnaire.py
@@ -1,16 +1,25 @@
-name = ''
+
 print("""Hello ''! Welcome to Carbon City, 
 where citizens strive each day to make more sustainable choices
  and reduce their carbon footprint""")
 print("To help us calculate your carbon footprint, please answer the following questions:")
 
 class Person:
-    def __init__(self, name, location, diet, energy_consumption, shopper_type):
+    def __init__(self, name, username, password, location, diet, energy_consumption, shopper_type):
         self.name = name
+        self.username = username
+        self.password = password
         self.location = location
         self.diet = diet
         self.energy_consumption = energy_consumption
         self.shopper_type = shopper_type
+
+# user creates account before questionnaire is presented
+# mock login system with username and password assumed correct
+    def login(self, username, password):
+        correct = True
+        while correct:
+            username - input()
 
     def welcome(self):
         print("Hello " + self.name + ". Welcome to Carbon City.")
